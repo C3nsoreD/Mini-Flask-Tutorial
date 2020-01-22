823e16c3831bb143d2bf8490f78732c2b6494b26
Gabriel <ur.gabriel@outlook.com>
2020-01-22 05:11:41 -0500

    Finisehd all blog views

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -34,9 +34,8 @@ def create_app(test_config=None):
 
     # Register the auth blueprint
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
 
-    app.redgister_blueprint(blog.bp)
-    app.add_url_rule("/", endpoint='index')
-
-    
+    app.add_url_rule('/', endpoint='index')
+        
     return app
